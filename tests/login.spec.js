e05ef6a82d407527c38f8360c0cd84b599bb1401
Subordinater <20511526-Subordinater@users.noreply.gitlab.com>
2025-05-14 18:12:38 +0100

    Initalise Login Test Specification

---
+++ tests/login.spec.js
@@ -0,0 +1,10 @@
+﻿import { test, expect } from '@playwright/test';
+
+test('Valid Login Credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+  await expect(page).toHaveURL(/inventory/);
+  await expect(page.locator('.inventory_list')).toBeVisible();
+});
